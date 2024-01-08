9997f61b78b26089a05636e10ead9209a8821b5a
mkdkck <colin173@gmail.com>
2024-01-08 13:49:55 +1030

    set models, added the helper function to formate the date and quantity

--- service.js
+++ service.js
@@ -4,14 +4,14 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const routes = require("./controllers");
-const auth = require("./utils/auth");
+const helpers = require('./utils/helpers');
 
 const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ auth });
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   cookie: {
